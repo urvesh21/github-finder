b6f5efe046a4f1a0216d75a3f74f921cca481de7
Urvesh Patel <urvesh21@gmail.com>
2020-04-05 14:43:44 -0400

    Added axios for http calls and refactored user components to show data coming from api

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layouts/Spinner';
+import PropTypes from 'prop-types';
 
-export default class Users extends Component {
-	render() {
+const Users = ({ users, loading }) => {
+	if (loading) {
+		return <Spinner />
+	} else {
 		return (
 			<div style={userStyles}>
-				{this.props.users.map((user) => (
+				{users.map((user) => (
 					<UserItem key={user.id} user={user} />
 				))}
 			</div>
@@ -13,8 +17,15 @@ export default class Users extends Component {
 	}
 }
 
+Users.propType = {
+	users: PropTypes.array.isRequired,
+	loading: PropTypes.bool.isRequired
+}
+
 const userStyles = {
 	display: 'grid',
 	gridTemplateColumns: 'repeat(3, 1fr)',
 	gridGap: '1rem'
 }
+
+export default Users;
