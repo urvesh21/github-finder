1bc6a77393174d2d950680b9ccae4a2afd2fbf21
Urvesh Patel <urvesh21@gmail.com>
2020-04-05 15:29:47 -0400

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
 import './App.css';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 
 class App extends Component {
@@ -11,10 +12,16 @@ class App extends Component {
 		loading: false
 	}
 
-	async componentDidMount() {
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
+	// 	const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	searchUsers = async (text) => {
 		this.setState({ loading: true });
-		const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-		this.setState({ users: res.data, loading: false });
+		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+		this.setState({ users: res.data.items, loading: false });
 	}
 
 	render() {
@@ -22,6 +29,7 @@ class App extends Component {
 			<div className="App">
 				<Navbar />
 				<div className="container">
+					<Search searchUsers={this.searchUsers} />
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
